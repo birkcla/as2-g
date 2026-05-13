d2d4695569b5b12db7feffb7e73a10d8d2c7ff71
B.C <none>
2026-05-13 22:34:07 +0200

    updated build

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/as2-graph-viewer/',
+  base: './',
   build: {
     outDir: 'docs'
   }
